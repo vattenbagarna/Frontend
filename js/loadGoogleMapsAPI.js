a179a2b715746c3c3ade2728c7a2c6ef74c7ff05
JohanDjarvKarltorp <johan.djarv.karltorp@gmail.com>
2019-02-22 16:23:13 +0100

    added html, css and js to print page

---
+++ js/loadGoogleMapsAPI.js
@@ -0,0 +1,5 @@
+function getKey() {
+  var script = document.createElement('script');
+  script.src = "https://maps.googleapis.com/maps/api/js?key=" + key + "&callback=myMap";
+  document.head.appendChild(script);
+}
