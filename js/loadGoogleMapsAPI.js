8942f00d66cce0ff5f3c72dc3d01e4542c7bb98d
JohanDjarvKarltorp <johan.djarv.karltorp@gmail.com>
2019-02-22 16:26:35 +0100

    updated syntax

--- js/loadGoogleMapsAPI.js
+++ js/loadGoogleMapsAPI.js
@@ -1,5 +1,6 @@
-function getKey() {
-  var script = document.createElement('script');
-  script.src = "https://maps.googleapis.com/maps/api/js?key=" + key + "&callback=myMap";
-  document.head.appendChild(script);
+function getKey () {
+    const script = document.createElement("script");
+
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${key}&callback=myMap`;
+    document.head.appendChild(script);
 }
