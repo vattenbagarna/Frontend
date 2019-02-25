2070655963789f1c7a01b69c3ea094186258a97c
JohanDjarvKarltorp <johan.djarv.karltorp@gmail.com>
2019-02-25 14:57:20 +0100

    updated to remove ESLINT errors

--- js/loadGoogleMapsAPI.js
+++ js/loadGoogleMapsAPI.js
@@ -1,20 +1,28 @@
-/* global document*/
+/* global document google*/
 
 import {
-  key
+    key
 } from "./getKey.js";
 
 const scri = document.createElement("script");
 
+/**
+ * Load google map from API into HTML
+ * @returns {null} no need to return anything else
+ */
+const myMap = () => {
+    const mapProp = {
+        "center": new google.maps.LatLng(56.16156, 15.58661),
+        "zoom": 15
+    };
+    const map = new google.maps.Map(
+        document.getElementById("printMap"),
+        mapProp
+    );
+
+    return map;
+};
+
+
 scri.src = `https://maps.googleapis.com/maps/api/js?key=${key}&callback=myMap`;
 document.head.appendChild(scri).addEventListener("load", myMap);
-
-
-
-function myMap() {
-  var mapProp = {
-    center: new google.maps.LatLng(56.16156, 15.58661),
-    zoom: 15,
-  };
-  var map = new google.maps.Map(document.getElementById("printMap"), mapProp);
-}
