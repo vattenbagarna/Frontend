9481938d431172939ae5415883618477799f2bbd
JohanDjarvKarltorp <johan.djarv.karltorp@gmail.com>
2019-02-25 14:15:10 +0100

    updated style and js structure

--- js/loadGoogleMapsAPI.js
+++ js/loadGoogleMapsAPI.js
@@ -1,6 +1,20 @@
-function getKey () {
-    const script = document.createElement("script");
+/* global document*/
 
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${key}&callback=myMap`;
-    document.head.appendChild(script);
+import {
+  key
+} from "./getKey.js";
+
+const scri = document.createElement("script");
+
+scri.src = `https://maps.googleapis.com/maps/api/js?key=${key}&callback=myMap`;
+document.head.appendChild(scri).addEventListener("load", myMap);
+
+
+
+function myMap() {
+  var mapProp = {
+    center: new google.maps.LatLng(56.16156, 15.58661),
+    zoom: 15,
+  };
+  var map = new google.maps.Map(document.getElementById("printMap"), mapProp);
 }
