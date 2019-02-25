f23cfff20d81ca5e17599a66204cef3fb8f21a5b
SpaceLenore <libreterra@protonmail.com>
2019-02-25 15:58:32 +0100

    added print css. fixed indentation

--- js/loadGoogleMapsAPI.js
+++ js/loadGoogleMapsAPI.js
@@ -1,7 +1,7 @@
 /* global document google*/
 
 import {
-  key
+    key
 } from "./getKey.js";
 
 const scri = document.createElement("script");
@@ -11,15 +11,17 @@ const scri = document.createElement("script");
  * @returns {map} no real reason why but it removes ESLINT error
  */
 const myMap = () => {
-  var mapProp = {
-    center: new google.maps.LatLng(56.16156, 15.58661),
-    zoom: 15,
-  };
-  let map = new google.maps.Map(document.getElementById("printMap"),
-    mapProp);
+    const mapProp = {
+        "center": new google.maps.LatLng(56.16156, 15.58661),
+        "zoom": 15
+    };
+    const map = new google.maps.Map(
+        document.getElementById("printMap"),
+        mapProp
+    );
 
-  return map;
-}
+    return map;
+};
 
 
 scri.src = `https://maps.googleapis.com/maps/api/js?key=${key}&callback=myMap`;
