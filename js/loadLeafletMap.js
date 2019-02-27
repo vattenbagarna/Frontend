228b6b26774d1c66a67e8105a77ded97de87d6b2
Sebastian Kotljarevski <kvakod@Sebastians-MacBook-Air.local>
2019-02-27 10:15:24 +0100

    idk

--- js/loadLeafletMap.js
+++ js/loadLeafletMap.js
@@ -7,7 +7,7 @@ let active = "";
 // Initialize the map
 const map = L.map("map", {
     "center": [51.505, -0.09],
-    "zoom": 10
+    "zoom": 10,
 });
 
 L.tileLayer(`https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=${key}`, {
