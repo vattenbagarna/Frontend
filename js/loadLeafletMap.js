94a1ee6bb0e74c3fffba7aa0871ab0d56db77ca9
Sebastian Kotljarevski <kvakod20@gmail.com>
2019-03-05 16:29:18 +0100

    User can now change between roadmap and satellite

--- js/loadLeafletMap.js
+++ js/loadLeafletMap.js
@@ -19,14 +19,25 @@ export const map = L.map("map", {
 
 object.search();
 
-const scri = document.createElement("script");
+const script = document.createElement("script");
 
-scri.src = `https://maps.googleapis.com/maps/api/js?key=${key}`;
-document.head.appendChild(scri).addEventListener("load", map);
+script.src = `https://maps.googleapis.com/maps/api/js?key=${key}`;
+document.head.appendChild(script);
 
-var roads = L.gridLayer.googleMutant({
-    type: 'satellite'	// valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
+var roadmap = L.gridLayer.googleMutant({
+    type: "roadmap"
 }).addTo(map);
+
+var satellite = L.gridLayer.googleMutant({
+    type: "satellite"
+});
+
+var baseMaps = {
+    "Karta": roadmap,
+    "Satellit": satellite
+};
+
+L.control.layers(baseMaps).addTo(map);
 
 for (let i = 0; i < document.getElementsByClassName("item").length; i++) {
     document.getElementsByClassName("item")[i].addEventListener("click", () => {
