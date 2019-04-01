f53f785f445d2badb937fb926818c4268d5734d4
Sebastian Kotljarevski <kvakod20@gmail.com>
2019-04-01 16:57:24 +0200

    Added map to materiallist view

---
+++ js/loadLeafletMaterialMap.js
@@ -0,0 +1,47 @@
+// Imports Google maps javascript api key from getKey.js file
+import {
+    key
+} from "./getKey.js";
+
+// Initialize the map with center coordinates on BAGA HQ and zoom 18.
+export const map = L.map("myMaterialMap", {
+    "center": [56.208640, 15.632630],
+    "editable": true,
+    "zoom": 18
+});
+
+// Creates script link to Google Maps javascript API with our key
+// then append it to head of map.html.
+const script = document.createElement("script");
+
+script.src = `https://maps.googleapis.com/maps/api/js?key=${key}`;
+document.head.appendChild(script);
+
+/**
+ * gridlayers - Creates the layer button in the top right corner with roadmap
+ * 				and satellite alternatives
+ *
+ * @returns {void}
+ */
+let gridlayers = () => {
+    // Creates Google maps roadmap (standard) grid layer and displays it on the map
+    var roadmap = L.gridLayer.googleMutant({
+        type: "roadmap"
+    }).addTo(map);
+
+    // Creates Google maps satellite grid layer
+    var satellite = L.gridLayer.googleMutant({
+        type: "satellite"
+    });
+
+    // Creates a array with roadmap and satellite inside and then add the array to a
+    // new control layer and displays it to the map in the top right corner.
+    var baseMaps = {
+        "Karta": roadmap,
+        "Satellit": satellite
+    };
+
+    L.control.layers(baseMaps).addTo(map);
+};
+
+gridlayers();
