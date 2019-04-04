a93e997a16819d1fce8d8c05fd71421197535771
eskoglund <eric.v.skoglund@gmail.com>
2019-04-04 15:32:58 +0200

    changed the name from "inches" to tum

--- js/flowCalc.js
+++ js/flowCalc.js
@@ -4,11 +4,11 @@
     * @returns {void}
     */
 function hideFlow() {
-  let showFlow = document.getElementById("flow");
-  let showSubmit = document.getElementById("submit");
+    let showFlow = document.getElementById("flow");
+    let showSubmit = document.getElementById("submit");
 
-  showSubmit.style.display = "none";
-  showFlow.style.display = "none";
+    showSubmit.style.display = "none";
+    showFlow.style.display = "none";
 }
 hideFlow();
 
@@ -18,14 +18,13 @@ hideFlow();
     * @param {submit} click
     * @returns {void}
     */
-document.getElementById("submit").addEventListener("click", () => {
-    let diameter = document.getElementById("length").value;
-    let length = document.getElementById("length").value;
-    let height = document.getElementById("height").value;
-    //let dimension = document.getElementById("dimension").value;
-    let flow = document.getElementById("flow").value;
-
-});
+// document.getElementById("submit").addEventListener("click", () => {
+//     let diameter = document.getElementById("length").value;
+//     let length = document.getElementById("length").value;
+//     let height = document.getElementById("height").value;
+//     //let dimension = document.getElementById("dimension").value;
+//     let flow = document.getElementById("flow").value;
+// });
 
 /**
     * Displays the outer dimension for the different materials after the outerdimension
@@ -35,8 +34,6 @@ document.getElementById("submit").addEventListener("click", () => {
     * @returns {void}
     */
 document.getElementById("outerdimension").addEventListener("click", () => {
-    let form = document.getElementsByClassName("form-inline")[0];
-
     if (document.getElementById("material").value === "PEM") {
         PEMPipe();
         document.getElementById("selectDim").style.display = "block";
@@ -60,8 +57,6 @@ document.getElementById("outerdimension").addEventListener("click", () => {
     * @returns {void}
     */
 document.getElementById("innerdimension").addEventListener("click", () => {
-    let form = document.getElementsByClassName("form-inline")[0];
-
     if (document.getElementById("material").value === "PEM") {
         PEMPipe();
         document.getElementById("selectDim").style.display = "block";
@@ -85,8 +80,6 @@ document.getElementById("innerdimension").addEventListener("click", () => {
     * @returns {void}
     */
 document.getElementById("inches").addEventListener("click", () => {
-    let form = document.getElementsByClassName("form-inline")[0];
-
     if (document.getElementById("material").value === "stainless") {
         stainlessPipe();
         document.getElementById("selectDim").style.display = "block";
@@ -102,12 +95,12 @@ document.getElementById("inches").addEventListener("click", () => {
     * @returns {void}
     */
 document.getElementById("material").addEventListener("change", () => {
-
     if (document.getElementById("material").value === "stainless") {
         document.getElementById("inches").style.display = "block";
-        document.getElementById("inches").nextSibling.innerHTML = "Inches";
+        document.getElementById("inches").nextSibling.innerHTML = "Tum";
     }
-    if (document.getElementById("material").value === "PEM" || document.getElementById("material").value === "PE") {
+    if (document.getElementById("material").value === "PEM" ||
+        document.getElementById("material").value === "PE") {
         document.getElementById("inches").style.display = "none";
         document.getElementById("inches").nextSibling.innerHTML = "";
     }
@@ -120,31 +113,33 @@ document.getElementById("material").addEventListener("change", () => {
     * @returns {void}
     */
 function PEMPipe() {
-  let outerDimPEM = ["40 mm", "50 mm", "60 mm"];
-  let innerDimPEM = ["35.2 mm", "44 mm", "55.4 mm"];
-  let select = document.getElementById("selectDim");
+    let outerDimPEM = ["40 mm", "50 mm", "60 mm"];
+    let innerDimPEM = ["35.2 mm", "44 mm", "55.4 mm"];
+    let select = document.getElementById("selectDim");
 
-  for (let i = select.childNodes.length - 1; i >= 0; i--) {
-      select.removeChild(select.childNodes[i]);
-  }
+    for (let i = select.childNodes.length - 1; i >= 0; i--) {
+        select.removeChild(select.childNodes[i]);
+    }
 
-  if (document.getElementById("outerdimension").checked) {
-    for (let i = 0; i < select.childNodes.length; i++) {
-        select.removeChild(select.childNodes[0]);
+    if (document.getElementById("outerdimension").checked) {
+        for (let i = 0; i < select.childNodes.length; i++) {
+            select.removeChild(select.childNodes[0]);
+        }
+        for (let i = 0; i < outerDimPEM.length; i++) {
+            let option = document.createElement("option");
+
+            option.innerHTML = outerDimPEM[i];
+            select.appendChild(option);
+        }
     }
-    for (let i = 0; i < outerDimPEM.length; i++) {
-        let option = document.createElement("option");
-        option.innerHTML = outerDimPEM[i];
-        select.appendChild(option);
+    if (document.getElementById("innerdimension").checked) {
+        for (let i = 0; i < innerDimPEM.length; i++) {
+            let option = document.createElement("option");
+
+            option.innerHTML = innerDimPEM[i];
+            select.appendChild(option);
+        }
     }
-  }
-  if (document.getElementById("innerdimension").checked) {
-    for (let i = 0; i < innerDimPEM.length; i++) {
-        let option = document.createElement("option");
-        option.innerHTML = innerDimPEM[i];
-        select.appendChild(option);
-    }
-  }
 }
 
 /**
@@ -153,28 +148,32 @@ function PEMPipe() {
     * @returns {void}
     */
 function PEPipe() {
-  let outerDimPE = ["110 mm", "160 mm", "180 mm", "200 mm", "225 mm", "250 mm", "315 mm", "400 mm", "450 mm", "500 mm"];
-  let innerDimPE = ["96.8 mm", "141 mm", "158.6 mm", "176.2 mm", "198.2 mm", "220.4 mm", "277.6 mm", "352.6 mm", "396.6 mm", "440.6 mm"];
-  let select = document.getElementById("selectDim");
+    let outerDimPE = ["110 mm", "160 mm", "180 mm", "200 mm", "225 mm", "250 mm",
+        "315 mm", "400 mm", "450 mm", "500 mm"];
+    let innerDimPE = ["96.8 mm", "141 mm", "158.6 mm", "176.2 mm", "198.2 mm",
+        "220.4 mm", "277.6 mm", "352.6 mm", "396.6 mm", "440.6 mm"];
+    let select = document.getElementById("selectDim");
 
-  for (let i = select.childNodes.length - 1; i >= 0; i--) {
-      select.removeChild(select.childNodes[i]);
-  }
+    for (let i = select.childNodes.length - 1; i >= 0; i--) {
+        select.removeChild(select.childNodes[i]);
+    }
 
-  if (document.getElementById("outerdimension").checked) {
-    for (let i = 0; i < outerDimPE.length; i++) {
-        let option = document.createElement("option");
-        option.innerHTML = outerDimPE[i];
-        select.appendChild(option);
+    if (document.getElementById("outerdimension").checked) {
+        for (let i = 0; i < outerDimPE.length; i++) {
+            let option = document.createElement("option");
+
+            option.innerHTML = outerDimPE[i];
+            select.appendChild(option);
+        }
     }
-  }
-  if (document.getElementById("innerdimension").checked) {
-    for (let i = 0; i < innerDimPE.length; i++) {
-        let option = document.createElement("option");
-        option.innerHTML = innerDimPE[i];
-        select.appendChild(option);
+    if (document.getElementById("innerdimension").checked) {
+        for (let i = 0; i < innerDimPE.length; i++) {
+            let option = document.createElement("option");
+
+            option.innerHTML = innerDimPE[i];
+            select.appendChild(option);
+        }
     }
-  }
 }
 
 /**
@@ -183,36 +182,39 @@ function PEPipe() {
     * @returns {void}
     */
 function stainlessPipe() {
-  let inches = ['1.25 "', '1.5 "', '2 "', '2.5 "'];
-  let outerDimStain = ["42.4 mm", "48.3 mm", "60.3 mm", "76.1 mm"];
-  let innerDimStain = ["38.4 mm", "44.3 mm", "56.3 mm", "72.1 mm", "80 mm", "100 mm", "150 mm"];
-  let select = document.getElementById("selectDim");
+    let inches = ['1.25 "', '1.5 "', '2 "', '2.5 "'];
+    let outerDimStain = ["42.4 mm", "48.3 mm", "60.3 mm", "76.1 mm"];
+    let innerDimStain = ["38.4 mm", "44.3 mm", "56.3 mm", "72.1 mm", "80 mm", "100 mm", "150 mm"];
+    let select = document.getElementById("selectDim");
 
-  for (let i = select.childNodes.length - 1; i >= 0; i--) {
-      select.removeChild(select.childNodes[i]);
-  }
+    for (let i = select.childNodes.length - 1; i >= 0; i--) {
+        select.removeChild(select.childNodes[i]);
+    }
 
-  if (document.getElementById("outerdimension").checked) {
-    for (let i = 0; i < outerDimStain.length; i++) {
-        let option = document.createElement("option");
-        option.innerHTML = outerDimStain[i];
-        select.appendChild(option);
+    if (document.getElementById("outerdimension").checked) {
+        for (let i = 0; i < outerDimStain.length; i++) {
+            let option = document.createElement("option");
+
+            option.innerHTML = outerDimStain[i];
+            select.appendChild(option);
+        }
     }
-  }
-  if (document.getElementById("innerdimension").checked) {
-    for (let i = 0; i < innerDimStain.length; i++) {
-        let option = document.createElement("option");
-        option.innerHTML = innerDimStain[i];
-        select.appendChild(option);
+    if (document.getElementById("innerdimension").checked) {
+        for (let i = 0; i < innerDimStain.length; i++) {
+            let option = document.createElement("option");
+
+            option.innerHTML = innerDimStain[i];
+            select.appendChild(option);
+        }
     }
-  }
-  if (document.getElementById("inches").checked) {
-    for (let i = 0; i < inches.length; i++) {
-        let option = document.createElement("option");
-        option.innerHTML = inches[i];
-        select.appendChild(option);
+    if (document.getElementById("inches").checked) {
+        for (let i = 0; i < inches.length; i++) {
+            let option = document.createElement("option");
+
+            option.innerHTML = inches[i];
+            select.appendChild(option);
+        }
     }
-  }
 }
 
 /**
@@ -221,43 +223,43 @@ function stainlessPipe() {
     * @returns {void}
     */
 function showStyling() {
-  document.getElementById("pressure1").style.display = "block";
-  document.getElementById("pressure2").style.display = "block";
-  document.getElementById("flow").style.display = "block";
-  document.getElementById("submit").style.display = "block";
-  document.getElementById("pressure1").nextSibling.innerHTML = "PN 6.3";
-  document.getElementById("pressure2").nextSibling.innerHTML = "PN 10";
-  document.getElementById("flowLabel").style.marginTop = "20px";
-  document.getElementById("flowLabel").innerHTML = "Flödeskapacitet";
-  document.getElementById("flow").style.marginRight = "10px";
-  document.getElementById("flow").nextSibling.innerHTML = "l/s";
-  document.getElementById("submit").nextSibling.innerHTML = "Punkförsluster";
+    document.getElementById("pressure1").style.display = "block";
+    document.getElementById("pressure2").style.display = "block";
+    document.getElementById("flow").style.display = "block";
+    document.getElementById("submit").style.display = "block";
+    document.getElementById("pressure1").nextSibling.innerHTML = "PN 6.3";
+    document.getElementById("pressure2").nextSibling.innerHTML = "PN 10";
+    document.getElementById("flowLabel").style.marginTop = "20px";
+    document.getElementById("flowLabel").innerHTML = "Flödeskapacitet";
+    document.getElementById("flow").style.marginRight = "10px";
+    document.getElementById("flow").nextSibling.innerHTML = "l/s";
+    document.getElementById("submit").nextSibling.innerHTML = "Punkförsluster";
 }
 
 /**
-    * hideStyling() - hides the input boxes 
+    * hideStyling() - hides the input boxes
     *
     * @returns {void}
     */
 function hideStyling() {
-  let select = document.getElementById("selectDim");
+    let select = document.getElementById("selectDim");
 
-  select.style.display = "none";
-  document.getElementById("innerdimension").style.display = "block";
-  document.getElementById("outerdimension").style.display = "block";
-  document.getElementById("innerdimension").nextSibling.innerHTML = "Innediameter";
-  document.getElementById("outerdimension").nextSibling.innerHTML = "Ytterdiameter";
-  document.getElementById("pressure1").style.display = "none";
-  document.getElementById("pressure2").style.display = "none";
-  document.getElementById("pressure1").nextSibling.innerHTML = "";
-  document.getElementById("pressure2").nextSibling.innerHTML = "";
-  document.getElementById("pressure1").checked = false;
-  document.getElementById("pressure2").checked = false;
-  document.getElementById("innerdimension").checked = false;
-  document.getElementById("outerdimension").checked = false;
-  document.getElementById("flowLabel").style.display = "none";
-  document.getElementById("flow").style.display = "none";
-  document.getElementById("submit").style.display = "none";
-  document.getElementById("flowLabel").innerHTML = "";
-  document.getElementById("flow").nextSibling.innerHTML = "";
+    select.style.display = "none";
+    document.getElementById("innerdimension").style.display = "block";
+    document.getElementById("outerdimension").style.display = "block";
+    document.getElementById("innerdimension").nextSibling.innerHTML = "Innediameter";
+    document.getElementById("outerdimension").nextSibling.innerHTML = "Ytterdiameter";
+    document.getElementById("pressure1").style.display = "none";
+    document.getElementById("pressure2").style.display = "none";
+    document.getElementById("pressure1").nextSibling.innerHTML = "";
+    document.getElementById("pressure2").nextSibling.innerHTML = "";
+    document.getElementById("pressure1").checked = false;
+    document.getElementById("pressure2").checked = false;
+    document.getElementById("innerdimension").checked = false;
+    document.getElementById("outerdimension").checked = false;
+    document.getElementById("flowLabel").style.display = "none";
+    document.getElementById("flow").style.display = "none";
+    document.getElementById("submit").style.display = "none";
+    document.getElementById("flowLabel").innerHTML = "";
+    document.getElementById("flow").nextSibling.innerHTML = "";
 }
