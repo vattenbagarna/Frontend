a401fd1356392d8366dc564cbc9b6e6ec6e3a168
Sebastian Kotljarevski <kvakod20@gmail.com>
2019-04-12 11:48:46 +0200

    Added the new project view and update project view with functionality.

--- js/newproject.js
+++ js/newproject.js
@@ -1,67 +1,62 @@
-
-let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Y2FjN2I4M2ZlZTg"
-+ "xMDg1YzdkMDI0Y2EiLCJ1c2VybmFtZSI6Imt2YWtvZEBnbWFpbC5jb20iLCJwYXNzd29yZCI6Ii"
-+ "QyYiQxMCRwODRKVGVVNmtsWjNPMVhTU0VuSDMuQTlnSGJsOGd6ZVl5YWcwVGVteS4zOThveHFBMD"
-+ "hmRyIsImlzQWRtaW4iOiJmYWxzZSIsImlhdCI6MTU1NDgwNzc0MH0.kifB6_hfqZ3_zJpnFsWLo"
-+ "CbMPiqF0rX8mm50OePZ06o";
-
-let button = document.getElementById("addButton");
-
-let x = 1;
-
-button.addEventListener("click", () => {
-    var selectArray = ["Sebastian", "Johan", "Axel", "Filip", "Erik", "Vidar"];
-    var optionArray = ["Läsbehörighet", "Skrivbehörighet"];
-
-    var selectUser = document.createElement("select");
-
-    selectUser.setAttribute("id", "accessSelect");
-
-    var newField = document.getElementById("newField");
-    var h = document.createElement("h3");
-    var t = document.createTextNode("Person " + x + ":");
-
-    h.appendChild(t);
-    newField.appendChild(h);
-    x = x + 1;
-
-    newField.appendChild(selectUser);
-
-    for (var i = 0; i < selectArray.length; i++) {
-        var option = document.createElement("option");
-
-        option.setAttribute("value", selectArray[i]);
-        option.text = selectArray[i];
-        selectUser.appendChild(option);
-    }
-
-    var selectCompetence = document.createElement("select");
-
-    selectCompetence.setAttribute("id", "accessCompetence");
-
-    newField.appendChild(selectCompetence);
-
-    for (i = 0; i < optionArray.length; i++) {
-        option = document.createElement("option");
-
-        option.setAttribute("value", optionArray[i]);
-        option.text = optionArray[i];
-        selectCompetence.appendChild(option);
-    }
-});
+/* eslint-disable max-len, no-unused-vars, no-undef */
+//Gets the users token
+let token = localStorage.getItem("token");
+let deleteButtonNumber = 0;
+let usernameObj = {};
+let usernameArray = [];
+let userIdArray = [];
+let optionArray = ["Läsbehörighet", "Skrivbehörighet"];
+let optionArrayValue = ["r", "w"];
 
 /**
- * createNewProject - Creates a new project and send it to the backend database with fetch.
+ * getAllUsers - Fetches all users from the database and creates a
+ * username object, username and userID array.
+ *
+ * @returns {json}
+ */
+let getAllUsers = () => {
+    let url = "http://localhost:1337/user/all?token=" + token;
+
+    fetch(url, {
+        method: 'GET',
+    }).then(response => {
+        return response.json();
+    }).then((json) => {
+        for (var i = 0; i < json.length; i++) {
+            usernameObj[json[i].id] = json[i].username;
+            usernameArray.push(json[i].username);
+            userIdArray.push(json[i].id);
+        }
+    });
+};
+
+/**
+ * createNewProject - Creates a new project and send it to the database using fetch.
  *
  * @returns {void}
  */
 let createNewProject = () => {
+    //Get all values from the form
     let projectName = document.getElementById("projectName").value;
+    let accessUser = document.getElementsByClassName("accessSelect");
+    let accessCompetence = document.getElementsByClassName("accessCompetence");
+    let peopleperhouse = document.getElementById("peopleperhouse").value;
+    let litreperperson = document.getElementById("litreperperson").value;
+    let accessData = "";
 
-    let data = "name=" + projectName + "&version=1.0";
+    //Loops through the amount of Users added in the newproject.html view and
+    //prepares to send them to the database.
+    for (var i = 0; i < accessUser.length; i++) {
+        accessData += `&access[${i}][userID]=${accessUser[i].value}&access[${i}][permission]=${accessCompetence[i].value}&access[${i}][username]=${usernameObj[accessUser[i].value]}&access[${i}]`;
+    }
 
-    let url = "http://localhost:1337/proj/insert/" + token + "?token=" + token;
+    //Adds the accessData with the rest of the data which needs to be sent to the database
+    let data = `name=${projectName}&version=1.0${accessData}&default[peoplePerHouse]=${peopleperhouse}&default[litrePerPerson]=${litreperperson}`;
 
+    //Url to call API/Backend
+    let url = "http://localhost:1337/proj/insert/12345678" + "?token=" + token;
+
+    //Fetch to post the data to the database
     fetch(url, {
         method: 'POST',
         body: data,
@@ -69,12 +64,111 @@ let createNewProject = () => {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
     }).then(res => res.json())
-        .then(response =>  location.href = "map.html?id=" + response[response.length - 1]._id)
-        .catch(error => console.error('Error:', error));
+        .then(response => console.log(response)/*location.href = "map.html?id=" + response[0]._id*/)
+        .catch(error => alert(error));
 };
 
+/**
+ * addRemoveButtons - Adds the buttons which remove users from newproject.html
+ *
+ * @returns {void}
+ */
+let addRemoveButtons = () => {
+    //Creates an a tag with a div wrapped around it which forms a button
+    let deleteDiv = document.createElement("div");
+
+    deleteDiv.setAttribute("class", "button-wrap removeDiv");
+    let deleteATag = document.createElement("a");
+
+    deleteATag.setAttribute("class", "button buttonNumber");
+    //Gives the button a value so it knows which user to remove when clicked
+    deleteATag.setAttribute("value", deleteButtonNumber);
+    deleteATag.innerHTML = "Ta bort";
+    deleteDiv.appendChild(deleteATag);
+    //Appends button to existing div
+    newField.appendChild(deleteDiv);
+
+    deleteButtonNumber = deleteButtonNumber + 1;
+
+    //Adds an eventListener to each button which removes the two select elements
+    //from newproject.html in turn also removes them from being added to db
+    deleteDiv.addEventListener("click", (event) => {
+        //sets the button numbers again when button is clicked so correct user
+        //is removed
+        setButtonNumber();
+        //gets the value of the button
+        let which = event.target.attributes[1].nodeValue;
+        //gets the html elements
+        let selectAccess = document.getElementsByClassName("accessSelect");
+        let selectCompetence = document.getElementsByClassName("accessCompetence");
+        let removeDiv = document.getElementsByClassName("removeDiv");
+        //removes the select elements and the button itself
+
+        removeDiv[which].remove();
+        selectAccess[which].remove();
+        selectCompetence[which].remove();
+    });
+};
+
+/**
+ * setButtonNumber - sets the button number for the remove buttons
+ *
+ * @returns {void}
+ */
+let setButtonNumber = () => {
+    //gets all buttons
+    let buttonNumber = document.getElementsByClassName("buttonNumber");
+
+    //loops all buttons and adds a number to its value
+    for (var i = 0; i < buttonNumber.length; i++) {
+        buttonNumber[i].setAttribute("value", i);
+    }
+};
+
+//Gets the HTML element for the button to add users
+let button = document.getElementById("addButton");
+
+//adds eventListener to add users(behörigheter) button
+button.addEventListener("click", () => {
+    //All this code adds the select elements and the button to remove them.
+    var selectUser = document.createElement("select");
+
+    selectUser.setAttribute("class", "accessSelect");
+
+    var newField = document.getElementById("newField");
+
+    newField.appendChild(selectUser);
+
+    //Adds options to the select
+    for (var i = 0; i < usernameArray.length; i++) {
+        var option = document.createElement("option");
+
+        option.setAttribute("value", userIdArray[i]);
+        option.text = usernameArray[i];
+        selectUser.appendChild(option);
+    }
+
+    var selectCompetence = document.createElement("select");
+
+    selectCompetence.setAttribute("class", "accessCompetence");
+
+    newField.appendChild(selectCompetence);
+
+    for (i = 0; i < optionArray.length; i++) {
+        option = document.createElement("option");
+
+        option.setAttribute("value", optionArrayValue[i]);
+        option.setAttribute("id", optionArray[i]);
+        option.text = optionArray[i];
+        selectCompetence.appendChild(option);
+    }
+    addRemoveButtons();
+});
+
+//gets the create project button
 let createProjectButton = document.getElementById("createProjectButton");
 
+//eventListener to create project button which calls createNewProject()
 createProjectButton.addEventListener("click", () => {
     createNewProject();
 });
