1bd21548684d4d2eec6968bd309a6af6ad138ead
SpaceLenore <libreterra@protonmail.com>
2019-05-06 17:37:44 +0200

    added change password route

---
+++ js/login/changepassword.js
@@ -0,0 +1,11 @@
+/* global configuration */
+
+"use strict";
+
+const errorHolder       = document.getElementById('error-holder');
+const form              = document.getElementsByClassName('pw-change-form');
+const pass              = document.getElementById('pass');
+const confirm           = document.getElementById('confirmpass');
+const user              = document.getElementById('uname')
+
+username.value = localStorage.getItem("username");
