e6cf9334986bf4178ea6bdaf5f334152b10d6c25
Glassinatorn <gusten1995@gmail.com>
2019-05-21 14:47:06 +0000

    added popup to capture invalid values

--- js/calculations/flowCalc.js
+++ js/calculations/flowCalc.js
@@ -234,10 +234,33 @@ function calcAll() {
     let roundPress = lostPress.toFixed(2);
     let roundTotal = totalPress.toFixed(2);
 
-    document.getElementById("flowSpeed").innerText = roundVel;
-    document.getElementById("staticPressure").innerText = height;
-    document.getElementById("pressureLoss").innerText = roundPress;
-    document.getElementById("totalPressure").innerText = roundTotal;
+	if (roundTotal == "NaN" || roundTotal < 0 ||
+		roundPress == "NaN" || roundPress < 0 ||
+		roundVel == "NaN" || roundVel < 0) {
+
+		if (roundTotal == "NaN" || roundTotal < 0) {
+			document.getElementById("totalPressure").innerText = "-";
+		}
+		if (roundPress == "NaN" || roundPress < 0) {
+			document.getElementById("pressureLoss").innerText = "-";
+		}
+		if (roundVel == "NaN" || roundVel < 0) {
+			document.getElementById("flowSpeed").innerText = "-";
+		}
+		if (height == "NaN") {
+			document.getElementById("staticPressure").innerText = "0";
+		} else {
+			document.getElementById("staticPressure").innerText = height;
+		}
+
+		alert("Ger ej ett dugligt värde");
+
+	} else {
+		document.getElementById("flowSpeed").innerText = roundVel;
+		document.getElementById("staticPressure").innerText = height;
+		document.getElementById("pressureLoss").innerText = roundPress;
+		document.getElementById("totalPressure").innerText = roundTotal;
+	}
 
     resetPumps();
 }
